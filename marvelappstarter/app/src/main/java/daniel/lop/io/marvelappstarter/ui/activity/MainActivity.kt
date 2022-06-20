462f856146699fe3842360067e8c827252e4c326
Android Up <thecodeturing@gmail.com>
2022-06-20 11:22:09 -0300

    first commit

---
+++ marvelappstarter/app/src/main/java/daniel/lop/io/marvelappstarter/ui/activity/MainActivity.kt
@@ -0,0 +1,35 @@
+package daniel.lop.io.marvelappstarter.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import dagger.hilt.android.AndroidEntryPoint
+import daniel.lop.io.marvelappstarter.R
+import daniel.lop.io.marvelappstarter.databinding.ActivityMainBinding
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews(binding)
+    }
+
+    private fun initViews(binding: ActivityMainBinding) {
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigation.apply {
+            setupWithNavController(navController)
+            setOnNavigationItemReselectedListener { }
+        }
+    }
+}
---
+++ marvelappstarter/app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -0,0 +1,17 @@
+package daniel.lop.io.marvelappstarter.util
+
+object Constants {
+
+    const val DATABASE_NAME = "marvel.db"
+    const val BASE_URL = "https://gateway.marvel.com/v1/public/"
+
+    const val PUBLIC_KEY = "b636f090efad253b7837ffd2d45a08ce"
+    const val PRIVATE_KEY = "440c47ce82ba7d10ba0a58a8228a3fb650ebd437"
+
+    const val LAST_SEARCH_QUERY = "last_search_query"
+    const val DEFAULT_QUERY = ""
+
+    const val APIKEY = "apikey"
+    const val HASH = "hash"
+    const val TS = "ts"
+}
